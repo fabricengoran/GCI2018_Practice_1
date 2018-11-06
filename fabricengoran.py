199660667f0663dbd1cbd5a2d5ed31c409ad4a62
fabricengoran <44637636+fabricengoran@users.noreply.github.com>
2018-11-06 18:41:22 +0100

    Update fabricengoran.py

--- fabricengoran.py
+++ fabricengoran.py
@@ -2,4 +2,4 @@ for i in range (10):
     print('GCI is great')
 name = input("What is your name? ")
 print("Please to meet you" +name+ "!")
-print("Did you know your name backwards is" +name [::-1])
+print("Did you know your name backwards is " +name [::-1])
